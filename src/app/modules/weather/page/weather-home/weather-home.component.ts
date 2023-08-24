63bf24dc1c680a9477e12abde0aa62dbc57a85ca
gguilhermelopes <guilhermelopes275@gmail.com>
2023-08-23 21:46:40 -0300

    feat: set form and bind with class properties

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Subject, takeUntil } from 'rxjs';
 
-import { WeatherService } from '../../services/weather.service';
 import { WeatherData } from 'src/app/models/interfaces/weather-data';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
-  private initialCityName = 'Jundiaí';
-  private weatherData!: WeatherData;
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  initialCityName = 'Jundiaí';
+  weatherData!: WeatherData;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) {}
 
@@ -19,12 +23,25 @@ export class WeatherHomeComponent implements OnInit {
   }
 
   getWeatherData(cityName: string): void {
-    this.weatherService.getWeatherData(cityName).subscribe({
-      next: (response) => {
-        response && (this.weatherData = response);
-        console.log(this.weatherData.name);
-      },
-      error: (error) => console.log(error),
-    });
+    this.weatherService
+      .getWeatherData(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherData = response);
+          console.log(this.weatherData.name);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  onSubmit(): void {
+    this.getWeatherData(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
